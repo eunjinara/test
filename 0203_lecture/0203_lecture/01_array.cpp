c7a3e8ee13aba13f528e609a76a3a8d2f4691370
eunjinara <zzsskkoopp@gmail.com>
2023-02-03 16:21:23 +0900

    study:배열

--- 0203_lecture/0203_lecture/01_array.cpp
+++ 0203_lecture/0203_lecture/01_array.cpp
@@ -1,5 +1,16 @@
-include <iostream>
+#include <iostream>
 
 int main() {
 
+	std::string friends[2] = { "È«±æµ¿","¼ºÃáÇâ" };
+	std::cout << friends[0];
+
+	double nums[3] = { 1.5, 2.5, 3.5 };
+
+	std::string fruit[2][3][2] = {
+		{ {"",""}, {"",""}, {"", ""} },
+		{{"",""}, {"",""}, {"", ""} }
+	};
+
+	return 0;
 }
